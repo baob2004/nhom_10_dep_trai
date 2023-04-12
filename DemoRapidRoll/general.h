1056e552862e0ec43dbd7dc2254c41d8d7b331fe
thanhdatho0 <85278412+thanhdatho0@users.noreply.github.com>
2023-04-13 05:01:19 +0700

    Update general.h

--- DemoRapidRoll/general.h
+++ DemoRapidRoll/general.h
@@ -3,8 +3,31 @@
 #include "random.h"
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 #define Width 700
-#define Height 600
+#define Height 800
+#define POINT 0;
+#define HEART 3;
 
 enum trangThai { UP, DOWN, LEFT, RIGHT };
+
+int h = HEART;
+int m = POINT;
+
+void the_end()
+{
+	int last_score = m/10;
+	int page = 0;
+	while (1)
+	{
+		setactivepage(page);
+		setvisualpage(1 - page);
+		cleardevice();
+		char c[50];
+		sprintf(c, "Your score: %d", last_score);
+		outtextxy(Width / 3, Height / 2, c);
+		page = 1 - page;
+		delay(20);
+	}
+}
