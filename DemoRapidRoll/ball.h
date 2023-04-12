093458ef1f8c3ac665696476e1089ab40533f59d
thanhdatho0 <85278412+thanhdatho0@users.noreply.github.com>
2023-04-13 04:58:04 +0700

    Update ball.h

--- DemoRapidRoll/ball.h
+++ DemoRapidRoll/ball.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "general.h"
 #include "bar.h"
+#include "fence.h"
 
 typedef struct ball
 {
@@ -9,46 +10,76 @@ typedef struct ball
 	int speed;
 }BALL;
 
+void create_ball(BALL* b);
+void ball_move(BALL* b, BAR* t, BAR* t2, BAR* t3, FENCE* f, FENCE* f2);
+void ball_control(BALL* b);
+int xu_ly(BALL* b, BAR* t);
+void xu_ly_cham_fence(BALL* b, FENCE* f);
+
 void create_ball(BALL* b)
 {
 	srand((int)time(0));
-	b->radius = 30;
+	b->radius = 20;
 	b->x = random(b->radius, Width - b->radius);
-	b->y = b->radius;
+	b->y = b->radius + 200;
 	b->speed = 5;
 }
 
-int xu_ly(BALL* b, BAR* t);
 
-void ball_move(BALL* b, BAR* t, BAR* t2, BAR* t3)
+void ball_move(BALL* b, BAR* t, BAR* t2, BAR* t3, FENCE *f, FENCE *f2)
 {
-	setcolor(RED);
-	setfillstyle(SOLID_FILL, RED);
+	setcolor(YELLOW);
+	setfillstyle(SOLID_FILL, YELLOW);
 	circle(b->x, b->y, b->radius);
-	floodfill(b->x, b->y, RED);
+	floodfill(b->x, b->y, YELLOW);
 
 	if (xu_ly(b, t) == UP || xu_ly(b, t2) == UP || xu_ly(b, t3) == UP)
 		b->y -= t->speed;
 	else b->y += b->speed;
 
-	if (b->y == Height - b->radius)
+	xu_ly_cham_fence(b, f);
+	xu_ly_cham_fence(b, f2);
+
+	if (b->y == Height - 20 - b->radius)
 	{
-		b->y = b->radius;
+		b->y = b->radius + 200;
 		b->x = random(b->radius, Width - b->radius);
+		h--;
+	}
+	else if (b->y <= b->radius + 100)
+	{
+		b->y = b->radius + 200;
+		b->x = random(b->radius, Width - b->radius);
+		h--;
 	}
 }
 
 void ball_control(BALL* b)
 {
 	if (GetAsyncKeyState(VK_LEFT))
-		b->x -= b->speed;
+		b->x -= (b->speed+2);
 	else if (GetAsyncKeyState(VK_RIGHT))
-		b->x += b->speed;
+		b->x += (b->speed+2);
+	if (b->x >= Width)
+		b->x = b->radius;
+	else if (b->x <= 0)
+		b->x = Width - b->radius;
 }
 
 int xu_ly(BALL* b, BAR* t)
 {
-	if ((b->x >= t->x && b->x <= t->x + 150) && b->y + b->radius == t->y - 30)
+	if ((b->x + 15 >= t->x && b->x - 15 <= t->x + 150) && (b->y + b->radius == t->y - 30 || b->y + b->radius == t->y - 25))
 		return UP;
 	else return DOWN;
+}
+
+void xu_ly_cham_fence(BALL* b, FENCE* f)
+{
+	if ((b->x + 15 >= f->x && b->x - 15 <= f->x + 150)
+		&& (b->y + b->radius == f->y - 25 || b->y + b->radius == f->y - 20))
+	{
+		b->x = random(b->radius, Width - b->radius);
+		b->y = b->radius + 200;
+		h--;
+	}
 }
