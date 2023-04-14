61a5ccaa331658a07313e9f31af7bf164e579c36
Đinh Quốc Bảo <62871705+baob2004@users.noreply.github.com>
2023-04-14 20:57:14 +0700

    Delete Rapid roll bao directory

--- Rapid roll bao/Tutorial.h
+++
@@ -1,6 +0,0 @@
-void Tutorial(){
-	settextstyle(SANS_SERIF_FONT, HORIZ_DIR, 4);
-    outtextxy(80,250,"AN MUI TEN <= HOAC => DE DY CHUYEN");
-    outtextxy(160,300,"CO GANG DUNG BI ROI NHE");
-   	delay(5000);
-}
