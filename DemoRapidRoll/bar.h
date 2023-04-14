89f7f96bf8fb2eec897e2bf20c6735200031d934
thanhdatho0 <85278412+thanhdatho0@users.noreply.github.com>
2023-04-15 03:05:55 +0700

    Update bar.h

--- DemoRapidRoll/bar.h
+++ DemoRapidRoll/bar.h
@@ -9,14 +9,12 @@ typedef struct bar
 }BAR;
 
 
-
 void create_bar(BAR* t);
 void create_bar2(BAR* t);
 void create_bar3(BAR* t);
 void bar_move(BAR* t);
 void bar_move2(BAR* t);
 void bar_move3(BAR* t);
-
 
 
 void create_bar(BAR* t)
@@ -31,11 +29,16 @@ void bar_move(BAR* t)
 {
 	setfillstyle(SOLID_FILL, WHITE);
 	bar(t->x, t->y - 20, t->x + 150, t->y);
+	if (m >= 15000 && m < 30000) t->speed = 6;
+	else if (m >= 30000 && m < 45000) t->speed = 7;
+	else if (m >= 45000 && m < 60000) t->speed = 8;
+	else if (m >= 60000 && m < 75000) t->speed = 9;
+	else if(m >= 75000)t->speed = 10;
 	if (t->y >= 0)
 		t->tt = UP;
 	if (t->tt == UP)
 		t->y -= t->speed;
-	if (t->y == 130)
+	if (t->y <= 130)
 	{
 		t->x = random(350, 550);
 		t->y = Height;
@@ -54,11 +57,16 @@ void bar_move2(BAR* t)
 {
 	setfillstyle(SOLID_FILL, WHITE);
 	bar(t->x, t->y - 20, t->x + 150, t->y);
+	if (m >= 15000 && m < 30000) t->speed = 6;
+	else if (m >= 30000 && m < 45000) t->speed = 7;
+	else if (m >= 45000 && m < 60000) t->speed = 8;
+	else if (m >= 60000 && m < 75000) t->speed = 9;
+	else if (m >= 75000)t->speed = 10;
 	if (t->y >= 0)
 		t->tt = UP;
 	if (t->tt == UP)
 		t->y -= t->speed;
-	if (t->y == 130)
+	if (t->y <= 130)
 	{
 		t->x = random(0, 200);
 		t->y = Height;
@@ -77,11 +85,16 @@ void bar_move3(BAR* t)
 {
 	setfillstyle(SOLID_FILL, WHITE);
 	bar(t->x, t->y - 20, t->x + 150, t->y);
+	if (m >= 15000 && m < 30000) t->speed = 6;
+	else if (m >= 30000 && m < 45000) t->speed = 7;
+	else if (m >= 45000 && m < 60000) t->speed = 8;
+	else if (m >= 60000 && m < 75000) t->speed = 9;
+	else if (m >= 75000)t->speed = 10;
 	if (t->y >= 0)
 		t->tt = UP;
 	if (t->tt == UP)
 		t->y -= t->speed;
-	if (t->y == 130)
+	if (t->y <= 130)
 	{
 		t->x = random(175, 375);
 		t->y = Height;
