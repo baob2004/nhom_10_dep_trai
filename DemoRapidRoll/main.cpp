f62ecece33a5b6fab7a10998b811b9295ddd503c
thanhdatho0 <85278412+thanhdatho0@users.noreply.github.com>
2023-04-15 16:07:30 +0700

    Update main.cpp

--- DemoRapidRoll/main.cpp
+++ DemoRapidRoll/main.cpp
@@ -24,28 +24,52 @@ int main()
 	create_fence(&f);
 	create_fence2(&f2);
 	int page = 0;
+fi_choice: menu();
 	while (1)
 	{
-		setactivepage(page);
-		setvisualpage(1 - page);
-		cleardevice();
-		mark(&m);
-		hearts(&h);
-		head_fence();
-		foot_fence();
-		ball_control(&b);
-		ball_move(&b, &t, &t2, &t3, &f, &f2);
-		bar_move(&t);
-		bar_move2(&t2);
-		bar_move3(&t3);
-		fence_move(&f);
-		fence_move(&f2);
-		page = 1 - page;
-		delay(20);
-		if (h == 0)
-			the_end();
+		if (GetAsyncKeyState(VK_LBUTTON)) {
+			int x = mousex();
+			int y = mousey();
+			if (x >= 206 && x <= 464 && y >= 171 && y <= 279)
+			{
+				while (1)
+				{
+					again:
+					setactivepage(page);
+					setvisualpage(1 - page);
+					cleardevice();
+					mark(&m);
+					hearts(&h);
+					head_fence();
+					foot_fence();
+					ball_control(&b);
+					ball_move(&b, &t, &t2, &t3, &f, &f2);
+					bar_move(&t);
+					bar_move2(&t2);
+					bar_move3(&t3);
+					fence_move(&f);
+					fence_move(&f2);
+					page = 1 - page;
+					delay(20);
+					if (h == 0)
+						the_end();
+					if (restart == 1) goto again;
+				}
+			}
+			else if (x >= 206 && x <= 464 && y >= 321 && y <= 429) {
+				cleardevice();
+				Tutorial();
+				cleardevice();
+				goto fi_choice;
+			}
+			if (x >= 206 && x <= 464 && y >= 471 && y <= 579) {
+				goto quit;
+			}
+		}
+			delay(20);
 	}
 	_getch();
+	quit:
 	closegraph();
 	return 0;
 }
