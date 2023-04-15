b7ff9f24ffe6826d85414e172ca15db1384969b3
thanhdatho0 <85278412+thanhdatho0@users.noreply.github.com>
2023-04-15 16:07:10 +0700

    Update heart.h

--- DemoRapidRoll/heart.h
+++ DemoRapidRoll/heart.h
@@ -10,8 +10,10 @@ void mark(int *m)
 	*m += crt_time->tm_sec;
 	sprintf_s(strTime, "%d", *m/10);
 	char p[10];
+	setcolor(WHITE);
+	settextstyle(SANS_SERIF_FONT, HORIZ_DIR, 3);
 	strcpy(p, "Mark: ");
-	outtextxy(550, 40, p);
+	outtextxy(500, 40, p);
 	outtextxy(600, 40, strTime);
 }
 
@@ -22,6 +24,7 @@ void hearts(int *heart)
 	circle(50, 50, 10);
 	floodfill(50, 50, YELLOW);
 	char c[10];
+	settextstyle(SANS_SERIF_FONT, HORIZ_DIR, 3);
 	sprintf_s(c, "%c  %d", 'X', *heart);
-	outtextxy(70, 42, c);
+	outtextxy(70, 40, c);
 }
