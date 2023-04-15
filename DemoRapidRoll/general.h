3d073450fa5f24d5da79fdb7e885d32fb2378ea9
thanhdatho0 <85278412+thanhdatho0@users.noreply.github.com>
2023-04-15 16:06:49 +0700

    Update general.h

--- DemoRapidRoll/general.h
+++ DemoRapidRoll/general.h
@@ -14,6 +14,7 @@ enum trangThai { UP, DOWN, LEFT, RIGHT };
 
 int h = HEART;
 int m = POINT;
+int restart = 0;
 
 void the_end()
 {
@@ -25,9 +26,79 @@ void the_end()
 		setvisualpage(1 - page);
 		cleardevice();
 		char c[50];
+		setcolor(WHITE);
+		settextstyle(SANS_SERIF_FONT, HORIZ_DIR, 3);
 		sprintf(c, "Your score: %d", last_score);
 		outtextxy(Width / 3, Height / 2, c);
+		strcpy(c, "Press enter to restart");
+		outtextxy(Width / 3 - 100, Height / 2 + 50, c);
+		strcpy(c, "Esc to quit");
+		outtextxy(Width / 3 - 100, Height / 2 + 100, c);
+		if (GetAsyncKeyState(VK_ESCAPE))
+		{
+			closegraph();
+		}
+		/*else if(GetAsyncKeyState(13))
+		{
+			restart = 1;
+		}*/
 		page = 1 - page;
 		delay(20);
 	}
+}
+
+void menu() {
+	srand(time(NULL));
+	char c[20];
+	cleardevice();
+	setcolor(RED);
+	settextstyle(SANS_SERIF_FONT, HORIZ_DIR, 7);
+	strcpy(c, "RAPID ROLL");
+	outtextxy(100, 50, c);
+	//	setcolor(4);
+	//    rectangle(200,150,500,600);
+	//  	rectangle(201,151,499,599);
+	setcolor(YELLOW);
+	settextstyle(SIMPLEX_FONT, HORIZ_DIR, 3);
+
+	rectangle(210, 175, 460, 275);
+	rectangle(209, 174, 461, 276);
+	rectangle(208, 173, 462, 277);
+	rectangle(207, 172, 463, 278);
+	rectangle(206, 171, 464, 279);
+	strcpy(c, "BAT DAU");
+	setcolor(WHITE);
+	outtextxy(265, 215, c);
+
+	setcolor(YELLOW);
+	rectangle(210, 325, 460, 425);
+	rectangle(209, 324, 461, 426);
+	rectangle(208, 323, 462, 427);
+	rectangle(207, 322, 463, 428);
+	rectangle(206, 321, 464, 429);
+	strcpy(c, "HUONG DAN");
+	setcolor(WHITE);
+	outtextxy(235, 365, c);
+
+	setcolor(YELLOW);
+	rectangle(210, 475, 460, 575);
+	rectangle(209, 474, 461, 576);
+	rectangle(208, 473, 462, 577);
+	rectangle(207, 472, 463, 578);
+	rectangle(206, 471, 464, 579);
+	strcpy(c, "THOAT GAME");
+	setcolor(WHITE);
+	outtextxy(225, 515, c);
+}
+
+void Tutorial() {
+	char c[50];
+	settextstyle(SANS_SERIF_FONT, HORIZ_DIR, 3);
+	strcpy(c, "AN MUI TEN <= HOAC => DE DI CHUYEN");
+	outtextxy(30, 250, c);
+	strcpy(c, "CO GANG DUNG BI ROI NHE");
+	outtextxy(120, 300, c);
+	strcpy(c, "DUNG CHAM VAO GAI");
+	outtextxy(150, 350, c);
+	delay(5000);
 }
